2affb9bcd0a36a6c2db1aa9057e190945c7f6002
root <root@srv636545.hstgr.cloud>
2024-12-14 15:21:43 +0000

    update

---
+++ 100.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// This program prints the numbers from 1 to 100
+
+int main(){
+for (int i = 1; i <= 100; i++) {
+        if (i % 2 == 0) {
+            cout << i << "\n";
+        }
+    }
+return 0;
+
+}   
+
