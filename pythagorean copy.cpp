78f5758540fd0f2ec79518d57f8b562b9ae44ae0
root <root@srv636545.hstgr.cloud>
2024-12-27 16:03:33 +0000

    module 1  end

---
+++ pythagorean copy.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// use cast to force data type
+
+int main(){
+
+int i;
+
+for(i=1; i<100; i++)
+cout << i  << " /2 is " << (float)i/2 << "\n";
+
+return 0;
+
+
+}
+
