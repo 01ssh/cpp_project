c458277d5252a82846a13a44a984722f53263439
root <root@srv636545.hstgr.cloud>
2024-12-27 10:44:43 +0000

    module 1  end

---
+++ module1_check_abs.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+
+double number, absolute;
+
+cout << " Enter a number : ";
+cin >> number;
+absolute = abs(number);
+
+cout << "The absolute value of your number is " << absolute;
+
+
+
+
+
+
+}
+
